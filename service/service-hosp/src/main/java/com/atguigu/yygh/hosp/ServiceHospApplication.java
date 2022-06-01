e3f0bb5471702e22195a920a07b2fcafde0a8e65
Nikecheng <n760144709@163.com>
2022-06-01 22:37:01 +0800

    医院模块

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nkc
+ * @date 22/6/1
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
